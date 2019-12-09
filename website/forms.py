af86d1d25c2f6ebd8f5fb4c8cfbf773bf6233c31
PooWoox <41601403+PooWoox@users.noreply.github.com>
2019-12-09 15:26:27 -0300

    Add files via upload

--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from website.models import Users
 from flask_login import current_user
@@ -47,3 +47,8 @@ class UpdateAccountForm(FlaskForm):
             user = Users.query.filter_by(username=email.data).first()
             if user:
                 raise ValidationError('Email already in use for an account.')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- website/routes.py
+++ website/routes.py
@@ -1,30 +1,17 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, abort
 from website import app, db, bcrypt
-from website.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from website.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from website.models import Users, Posts
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts = [
-    {
-        'author': 'Rodrigo Brochado',
-        'title': 'ACONCHEGO',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2019'
-    },
-    {
-        'author': 'Bob dog',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2019'
-    }
-]
 
 @app.route('/')
 @app.route('/home')
 def home():
+    posts = Posts.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -63,6 +50,7 @@ def login():
             flash('Email and password combination not found. Check your login information and try again.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
@@ -83,6 +71,7 @@ def save_picture(form_picture):
 
     return picture_fn
 
+
 @app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
@@ -101,3 +90,53 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pictures/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@app.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Posts(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post created successfully!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form, legend= 'New Post')
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Posts.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Posts.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Post update!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Edit Post', form=form, legend= 'Update Post')
+
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Posts.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted.', 'info')
+    return redirect(url_for('home'))
